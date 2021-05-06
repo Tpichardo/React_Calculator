b9209e34296532599e1ded5e094928b79dfa401c
Talia Pichardo <taliapichardo@gmail.com>
2021-05-06 16:30:14 -0400

    added commas

--- src/App.js
+++ src/App.js
@@ -7,40 +7,50 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      result: '',
+      result: '0',
     }
   }
 
   clickedVal = (value) => {
-    this.setState({
-      result: this.state.result + value
-    })
-
+    let { result } = this.state
+    result = result.replaceAll(',', '')
+    if (result !== '0') {
+      let num = result + value
+      this.setState({
+        result: (Number(num).toLocaleString().toString())
+      })
+    } else {
+      this.setState({
+        result: value
+      })
+    }
   }
 
   reset = () => {
     this.setState({
-      result: ''
+      result: '0'
     })
   }
 
   calculate = () => {
     this.setState({
-      result: (eval(this.state.result) || '') + ''
+      result: (eval(this.state.result).toLocaleString('en-US') || '') + ''
     })
   }
 
   percent = () => {
     this.setState({
-      result: this.state.result / 100
+      result: parseInt(this.state.result) / (100)
     })
   }
 
-  posneg = () => [
+  posneg = () => {
     this.setState({
-      result: this.state.result * (-1)
+      result: Number(this.state.result) * (-1)
     })
-  ]
+
+  }
+
 
   render() {
     const { result } = this.state
