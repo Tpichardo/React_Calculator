abce41c15bedae2ebd38f015a48f901cd879417a
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-05-12 11:32:57 -0400

    Update OutPut.js

--- src/Components/OutPut.js
+++ src/Components/OutPut.js
@@ -4,6 +4,7 @@ import './OutPut.css';
 const OutPut = ({ outPut }) => {
     return (
         <div className="OutPut">
+            {/* I think outPut is a string, not a number, which is why .toLocaleString() isn't inserting commas like we want */}
             <p>{outPut.toLocaleString('en-US')}</p>
         </div>
     )
