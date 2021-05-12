d08032e1ebea0b98a8e054e2d1aa3542fad1e121
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-05-12 11:42:07 -0400

    Update KeyPad.js

--- src/Components/KeyPad.js
+++ src/Components/KeyPad.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './KeyPad.css';
 
 const KeyPad = ({ clickedVal, reset, calculate, percent, posneg, sum, subtract, divide, multiply }) => {
+    // how could you refactor this so that it's more DRY?
+    // the code below is more repetitive and difficult to maintain than it needs to be
+    // For instance, if you need to make a change to the number button click handlers, right now you need to
+    // update 10 different functions!
+    
+    // Suggestion: write a single numberClick function. 
+    // then create a variable: const buttons = [{value: '4', onClick: numberClick, display: '4'}, ...]
+    // then use .map to create all your buttons.
     return (
         <div className="buttons">
             <button className='darkgreen' value='' onClick={() => reset()}>AC</button>
